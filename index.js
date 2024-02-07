705117db9d07e13346598be3f641590cb4031543
Aki Manninen <t0maak00@students.oamk.fi>
2024-02-07 12:51:40 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-require('dotenv').config();
+
 const express = require('express')
+const app = express()
 const cors = require('cors')
+require('dotenv').config();
+
 const Note = require('./models/note')
 
-const app = express()
-
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
+  console.log('\nMethod:', request.method)
   console.log('Path:  ', request.path)
   console.log('Body:  ', request.body)
   console.log('---')
   next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {    
+    return response.status(400).json({ error: error.message })  
+  } 
+
+  next(error)
 }
 
 app.use(express.json())
@@ -20,10 +37,6 @@ app.use(requestLogger)
 
 // not needed anymore - so it's empty []
 let notes = []
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-  })
   
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
@@ -41,8 +54,7 @@ app.get('/api/notes/:id', (request, response, next) => {
         response.status(404).end()      
       }    
     })
-    .catch(error => 
-      next(error))
+    .catch(error => next(error))
   })
 
 app.post('/api/notes', (request, response, next) => {
@@ -57,12 +69,11 @@ app.post('/api/notes', (request, response, next) => {
   .then(savedNote => {
     response.json(savedNote)
   })
-  .catch(error => 
-    next(error))
+  .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
+  Note.findByIdAndDelete(request.params.id,)
     .then(result => {
       response.status(204).end()
     })
@@ -70,39 +81,21 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id,     
+    { content, important },    
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedNote => {
       response.json(updatedNote)
     })
     .catch(error => next(error))
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
 // olemattomien osoitteiden käsittely
 app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {    
-    return response.status(400).json({ error: error.message })  
-  }
-
-  next(error)
-}
-
 // tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
 app.use(errorHandler)
 
